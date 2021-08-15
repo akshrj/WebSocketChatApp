c2740f56f7e76065018a6da9cef090406b5c1b3a
akshrj <akshrj4@gmail.com>
2021-08-15 09:54:14 +0530

    Update index.js
    add process.env.port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static("public"));
 
-const server = app.listen(8080, function() {
+const server = app.listen(process.env.PORT || 8080, function() {
 	console.log("Listening on port 8080")
 });
 
